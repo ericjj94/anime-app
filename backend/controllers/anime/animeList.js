edacf068476359b17d16199d8d9bb734cce826c3
unknown <eric.joy@mail.valuecoders.com>
2020-08-01 12:52:38 +0530

    created route for anime and reduced payload for animeList

--- backend/controllers/anime/animeList.js
+++ backend/controllers/anime/animeList.js
@@ -1,12 +1,19 @@
 
 const Watched = require('../../Models/watched');
 
-const itemsPerPage = 10;
-
-const animeList = async (req,res) => {
+const getAnimeList = async (req,res) => {
+    const itemsPerPage = 10;
     const currentPage = req.query && req.query.currentPage;
+    const fieldsToBeReturned = {name:1,_id:1,description:1,img:1};
     const skip = (currentPage - 1)* itemsPerPage;
-    let watchedAnimeList = await Watched.find({}).skip(skip).limit(itemsPerPage);
+    const watchedAnimeList = await Watched.find({},fieldsToBeReturned).skip(skip).limit(itemsPerPage);
     res.send({code: 200, data: watchedAnimeList })
 } 
-module.exports = animeList;
+
+const getAnimeDetails = async (req,res) => {
+    const id = req.query && req.query.id;
+    const selectedAnime = await Watched.find({_id: id});
+    res.send({code: 200, data: selectedAnime && selectedAnime[0]});
+}
+
+module.exports = { getAnimeList, getAnimeDetails};
